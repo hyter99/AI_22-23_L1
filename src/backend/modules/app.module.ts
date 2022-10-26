a7414b4de24e6ae3ced97ddcace73ea76dcf45f1
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2022-10-26 14:13:42 +0200

    be: add ServeStaticModule (#34)
    Add ServeStaticModule to AppModule (backend)

--- src/backend/modules/app.module.ts
+++ src/backend/modules/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { UniqueConstraint } from '../decorators/unique.decorator';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './database/prisma.module';
 
+@Global()
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '.vite'),
+      exclude: ['/api*'],
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
   providers: [UniqueConstraint],
 })
 export class AppModule {}
