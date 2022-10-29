5f8026fc4d462c81dbbfc9744059cc954021c91c
hyter99 <78922953+hyter99@users.noreply.github.com>
2022-10-29 17:00:11 +0200

    added login mechanism (#36)

---
+++ src/backend/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { AuthService } from '../auth.service';
+
+type TokenPayload = { userId: number };
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: TokenPayload) {
+    return this.authService.getById(payload.userId);
+  }
+}
