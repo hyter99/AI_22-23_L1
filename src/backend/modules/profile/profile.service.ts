a429a4e74c7652cb5bbd01bfa240ba28541268e3
Gabryjiel <gabriel.kudyba@gmail.com>
2022-11-06 21:17:15 +0100

    be: eslint && add getUserWallet endpoint

--- src/backend/modules/profile/profile.service.ts
+++ src/backend/modules/profile/profile.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../database/prisma.service";
-import { AddWalletDto } from "./dto/addWallet.dto";
-import { GetUserBuyOfferQuery } from "./dto/getUserBuyOffer.query";
-import { GetUserSellOfferQuery } from "./dto/getUserSellOffer.query";
-import { GetUserStockQuery } from "./dto/getUserStock.query";
-import { UpdateUserDto } from "./dto/updateUser.dto";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { AddWalletDto } from './dto/addWallet.dto';
+import { GetUserBuyOfferQuery } from './dto/getUserBuyOffer.query';
+import { GetUserSellOfferQuery } from './dto/getUserSellOffer.query';
+import { GetUserStockQuery } from './dto/getUserStock.query';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Injectable()
-export class ProfileService{
-  constructor(private readonly prisma: PrismaService) { }
-  
+export class ProfileService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getUserProfile(userId: number) {
-     return this.prisma.user.findFirstOrThrow({
+    return this.prisma.user.findFirstOrThrow({
       where: {
-      userId
-       },
-       select: {
-         userId: true,
-         email: true,
-         name: true,
-         surname: true,
-         balanceCents: true
-       }
-    })
+        userId,
+      },
+      select: {
+        userId: true,
+        email: true,
+        name: true,
+        surname: true,
+        balanceCents: true,
+      },
+    });
   }
 
   getUserStock(userId: number, getUserStockQuery: GetUserStockQuery) {
@@ -30,15 +30,15 @@ export class ProfileService{
       take: getUserStockQuery.take,
       skip: getUserStockQuery.skip,
       orderBy: {
-        [getUserStockQuery.orderBy]: getUserStockQuery.orderType
+        [getUserStockQuery.orderBy]: getUserStockQuery.orderType,
       },
       where: {
         userId,
         UserStockToStock: {
           StockToCompany: {
-            name: getUserStockQuery.companyName
-          }
-        }
+            name: getUserStockQuery.companyName,
+          },
+        },
       },
       select: {
         userStockId: true,
@@ -53,20 +53,23 @@ export class ProfileService{
               select: {
                 name: true,
                 description: true,
-              }
-            }
-          }
-        }
+              },
+            },
+          },
+        },
       },
-    })
+    });
   }
 
-  getUserSellOffers(userId: number, getUserSellOfferQuery: GetUserSellOfferQuery) {
+  getUserSellOffers(
+    userId: number,
+    getUserSellOfferQuery: GetUserSellOfferQuery,
+  ) {
     return this.prisma.sellOffer.findMany({
       take: getUserSellOfferQuery.take,
       skip: getUserSellOfferQuery.skip,
       orderBy: {
-        [getUserSellOfferQuery.orderBy]: getUserSellOfferQuery.orderType
+        [getUserSellOfferQuery.orderBy]: getUserSellOfferQuery.orderType,
       },
       where: {
         userId,
@@ -78,26 +81,29 @@ export class ProfileService{
         unitSellPriceCents: true,
         quantity: true,
         created: true,
-        status: true
-      }
-    })
+        status: true,
+      },
+    });
   }
 
-  getUserBuyOffers(userId: number, getUserSellOfferQuery: GetUserBuyOfferQuery) {
+  getUserBuyOffers(
+    userId: number,
+    getUserSellOfferQuery: GetUserBuyOfferQuery,
+  ) {
     return this.prisma.buyOffer.findMany({
       take: getUserSellOfferQuery.take,
       skip: getUserSellOfferQuery.skip,
       orderBy: {
-        [getUserSellOfferQuery.orderBy]: getUserSellOfferQuery.orderType
+        [getUserSellOfferQuery.orderBy]: getUserSellOfferQuery.orderType,
       },
       where: {
         userId,
         status: getUserSellOfferQuery.status,
         BuyOfferToStock: {
           StockToCompany: {
-            name: getUserSellOfferQuery.companyName
-          }
-        }
+            name: getUserSellOfferQuery.companyName,
+          },
+        },
       },
       select: {
         buyOfferId: true,
@@ -112,39 +118,50 @@ export class ProfileService{
             StockToCompany: {
               select: {
                 name: true,
-              }
-            }
-          }
-        }
-      }
-    })
+              },
+            },
+          },
+        },
+      },
+    });
   }
 
   updateUserAccount(userId: number, updateUserDto: UpdateUserDto) {
     return this.prisma.user.update({
       where: {
-        userId
+        userId,
       },
       data: updateUserDto,
       select: {
-        userId: true
-      }
-    })
+        userId: true,
+      },
+    });
   }
 
   addWallet(userId: number, addWalletDto: AddWalletDto) {
     return this.prisma.user.update({
       where: {
-        userId
+        userId,
       },
       data: {
         balanceCents: {
-          increment: addWalletDto.amountCents
-        }
+          increment: addWalletDto.amountCents,
+        },
       },
       select: {
-        userId: true
-      }
-    })
+        userId: true,
+      },
+    });
+  }
+
+  getUserWaller(userId: number) {
+    return this.prisma.user.findFirst({
+      where: {
+        userId,
+      },
+      select: {
+        balanceCents: true,
+      },
+    });
   }
 }
