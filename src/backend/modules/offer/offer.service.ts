75bae9215907907bfa243347950051f2667a575c
hyter99 <78922953+hyter99@users.noreply.github.com>
2022-12-04 21:35:35 +0100

    created mechanism of updating price with lowest price (#81)

--- src/backend/modules/offer/offer.service.ts
+++ src/backend/modules/offer/offer.service.ts
@@ -23,7 +23,32 @@ export class OfferService {
     });
   }
 
-  createSellOffer(createSellOfferDto: CreateSellOfferDto, userId: number) {
+  async createSellOffer(
+    createSellOfferDto: CreateSellOfferDto,
+    userId: number,
+  ) {
+    const company = await this.prisma.userStock.findFirstOrThrow({
+      select: { companyId: true },
+      where: { userStockId: createSellOfferDto.userStockId },
+    });
+
+    const lowestPrice = await this.prisma.companyStockPriceHistory.findFirst({
+      select: { priceCents: true, companyId: true },
+      where: { companyId: company.companyId },
+      orderBy: { priceCents: 'asc' },
+    });
+
+    if (
+      (lowestPrice?.priceCents ?? 0) < createSellOfferDto.unitSellPriceCents
+    ) {
+      await this.prisma.companyStockPriceHistory.create({
+        data: {
+          priceCents: createSellOfferDto.unitSellPriceCents,
+          companyId: company.companyId,
+        },
+      });
+    }
+
     return this.prisma.sellOffer.create({
       data: {
         userId: userId,
