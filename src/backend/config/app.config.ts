b83084e820a9b835f51a80711a5add3106881da4
pikrog <38695943+pikrog@users.noreply.github.com>
2022-12-11 12:29:51 +0100

    93 be change envars validation from zod to class validator (#101)
    * validate config with class-validator
    
    * adjust validation schema
    
    * remove unnecessary optional
    
    * refactor: decorator order, change optional to assert not null
    
    * uninstall zod
    
    Co-authored-by: Feargan <38695943+Feargan@users.noreply.github.com>

---
+++ src/backend/config/app.config.ts
@@ -0,0 +1,65 @@
+import {IsInt, IsNotEmpty, IsString, IsUrl, Max, Min, validateSync, ValidationError} from "class-validator"
+import {plainToInstance} from "class-transformer";
+
+export class AppConfig {
+  @IsUrl({
+    protocols: ["http", "https"],
+    requireProtocol: true,
+    requireHost: true,
+    require_tld: false,
+  })
+  VITE_BACKEND_URL!: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_CONNECTOR!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_USER!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_PASSWORD!: string;
+
+  @Min(1)
+  @Max(65535)
+  @IsInt()
+  DATABASE_PORT!: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_HOST!: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_URL!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET!: string;
+}
+
+class ViolatedConstraint {
+  [type: string]: string;
+}
+
+export class ConfigValidationError {
+  readonly message: string;
+  readonly constraints: ViolatedConstraint[];
+
+  constructor(violatedConstraints: ViolatedConstraint[]) {
+    this.message = "Config validation failed";
+    this.constraints = violatedConstraints;
+  }
+}
+
+export const validateConfig = (config: Record<string, any>) => {
+    const parsedConfig = plainToInstance(AppConfig, config, {enableImplicitConversion: true});
+    const errors = validateSync(parsedConfig);
+    if(errors.length !== 0) {
+      const violatedConstraints = errors.map((e: ValidationError) => e.constraints!);
+      throw new ConfigValidationError(violatedConstraints)
+    }
+    return parsedConfig;
+};
